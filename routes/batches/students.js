7a641d89279e138cb89921970d42317d8a2c0402
brunadafonseca <brunadafonseca@gmail.com>
2018-01-26 12:57:51 +0100

    Fix add students feature

--- routes/batches/students.js
+++ routes/batches/students.js
@@ -21,20 +21,20 @@ router
     res.json(req.batch.students)
   })
 
-  .get('/batches/:id/students/:studentId', loadBatch, (req, res, next) => {
+  .get('/batches/:id/students/:student_id', loadBatch, (req, res, next) => {
     if (!req.batch) { return next() }
     const student = req.batch.students.filter(student => {
-      return student._id.toString() === req.params.studentId
+      return student._id.toString() === req.params.student_id
     })[0]
-
     res.json(student)
   })
 
-  .patch('/batches/:id/students', authenticate, loadBatch, (req, res, next) => {
+  .post('/batches/:id/students', authenticate, loadBatch, (req, res, next) => {
     if (!req.batch) { return next() }
 
+    console.log(req.body)
     const newStudent = req.body
-    req.batch.students.push({ newStudent })
+    req.batch.students.push(newStudent)
 
     req.batch.save()
       .then((batch) => {
@@ -44,20 +44,24 @@ router
       .catch((error) => next(error))
     },
     (req, res, next) => {
-      res.json(req.students)
+      res.json(req.batch)
     })
 
-    .patch('/batches/:id/students/:studentId', loadBatch, authenticate, (req, res, next) => {
-      let student = req.batch.students.filter((student) => student._id.toString() === req.params.studentId)[0]
+    .patch('/batches/:id/students/:student_id', loadBatch, authenticate, (req, res, next) => {
+      if (!req.batch) { return next() }
+      console.log(req.batch)
+      const student = req.batch.students.filter(student => {
+        return student._id.toString() === req.params.student_id
+      })[0]
 
-      student.evaluations = student.evaluations.push(req.body)
-      console.log(student.evaluations)
+      console.log(student)
+      student.evaluations.push(req.body)
 
       req.batch.save()
         .then((batch) => {
           req.batch = batch
         })
-        res.json(req.batch)
+        res.json(currentStudent[0])
     })
     .delete('/batches/:id/students/:studentId', authenticate, (req, res, next) => {
       if (!req.batch) { return next() }
