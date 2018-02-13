dfce8581c52766b92ff0b9259b8d15ac037cc88d
brunadafonseca <brunadafonseca@gmail.com>
2018-02-13 21:58:28 +0100

    Clean up batch model

--- models/batch.js
+++ models/batch.js
@@ -1,14 +1,6 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
-// const evaluationSchema = new Schema({
-//   day: { type: Date, default: Date.now },
-//   color: { type: String },
-//   remark: { type: String },
-//   createdAt: { type: Date, default: Date.now },
-//   updatedAt: { type: Date, default: Date.now }
-// })
-
 const studentSchema = new Schema({
   name: { type: String, required: true },
   photo: { type: String, required: true },
